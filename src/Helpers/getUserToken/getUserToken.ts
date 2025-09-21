ea1c8059e4dbdeb86ef9355caf1d06882a3f12a6
Zainab Ali Mousa <safaali8118-email@example.com>
2025-09-21 03:55:39 +0300

    e

--- src/Helpers/getUserToken/getUserToken.ts
+++ src/Helpers/getUserToken/getUserToken.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-  const token = (await cookies()).get("token")?.value;
+  const token = (await cookies()).get("next-auth.csrf")?.value;
   return token || null;
 }
